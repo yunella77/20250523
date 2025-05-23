07a432e53bce80ee30bc7a601b4ec63faa00fe7c
yunella77 <413730374@o365.tku.edu.tw>
2025-05-23 10:00:14 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+let video;
+let facemesh;
+let predictions = [];
+const indices = [
+  409,270,269,267,0,37,39,40,185,61,146,91,181,84,17,
+  314,405,321,375,291,76,77,90,180,85,16,315,404,320,
+  307,306,408,304,303,302,11,72,73,74,184
+];
+
+function setup() {
+  createCanvas(640, 480).position(
+    (windowWidth - 640) / 2,
+    (windowHeight - 480) / 2
+  );
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
+
+  facemesh = ml5.facemesh(video, modelReady);
+  facemesh.on('predict', results => {
+    predictions = results;
+  });
+}
+
+function modelReady() {
+  console.log('Facemesh model loaded!');
+}
+
+function draw() {
+  image(video, 0, 0, width, height);
+
+  if (predictions.length > 0) {
+    const keypoints = predictions[0].scaledMesh;
+    stroke(255, 0, 0);
+    strokeWeight(15);
+    noFill();
+    beginShape();
+    for (let i = 0; i < indices.length; i++) {
+      const idx = indices[i];
+      const [x, y] = keypoints[idx];
+      vertex(x, y);
+    }
+    endShape();
+  }
+}
